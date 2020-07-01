e1ee897449863860a9ead726f050c510a79cdcc4
DarkWater <daliskandarov.ru@mail.ru>
2020-07-01 16:56:00 +0300

    fix

--- script.js
+++ script.js
@@ -4,13 +4,55 @@ let slider_button = document.querySelector(".slider_button");
 
 document.body.addEventListener("click", (event) =>{
     let target = event.target;
+    if(target.matches(".slider_item"))
+        slider_menu.classList.remove("slider_active");
     if(!target.matches(".slider_button"))
         return
     if(slider_menu.classList.contains("slider_active"))
         slider_menu.classList.remove("slider_active");
     else
         slider_menu.classList.add("slider_active");
+    
 })
+
+// Cache selectors
+var lastId,
+    topMenu = $(".slider_menu"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href='#"+id+"']").parent().addClass("active");
+   }                   
+});
+
 document.addEventListener("DOMContentLoaded", function(){
     var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
     console.log(scrollbar);
